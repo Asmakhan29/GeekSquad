c871d1e42d482bf5f6daed2ce1b39343fba2c1f6
m-triple-m <triplem656@gmail.com>
2024-02-03 10:34:54 +0530

    otp added

--- frontend/src/components/UserAuthenticate.jsx
+++ frontend/src/components/UserAuthenticate.jsx
@@ -71,7 +71,7 @@ const UserAuthenticate = () => {
           </div>
         </div>
         <button
-        onClick={addUser}
+          onClick={sendOTP}
           type="submit"
           className="mt-5 flex w-full justify-center rounded-3xl bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
